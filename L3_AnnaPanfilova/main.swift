35146a2a920c4009ecd9a75eae5152ede9e6aad2
Anna <anna.panfilova2107@gmail.com>
2020-07-24 12:43:24 +0300

    Initial Commit

---
+++ L3_AnnaPanfilova/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L3_AnnaPanfilova
+//
+//  Created by Anna on 24.07.2020.
+//  Copyright © 2020 Anna. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
